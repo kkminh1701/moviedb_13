45ffca5ee3069e565abaea6be4c0d27640bedd90
kkminh1701 <nguyencongminh1221@gmail.com>
2018-06-18 20:44:20 +0700

    Task3_create_UI_home

--- app/src/main/java/com/framgia/moviedb_13/screen/home/HomeActivity.java
+++ app/src/main/java/com/framgia/moviedb_13/screen/home/HomeActivity.java
@@ -1,13 +1,13 @@
-package com.framgia.moviedb_13;
+package com.framgia.moviedb_13.screen.home;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import com.framgia.moviedb_13.R;
 
-public class MainActivity extends AppCompatActivity {
-
+public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
